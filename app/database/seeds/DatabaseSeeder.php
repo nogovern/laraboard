bec390f405b4ae87e8799c911023dc0144553c90
장광희 <nogovern@gmail.com>
2014-12-29 04:47:33 +0000

    articles, posts 테스트용 seed 추가

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('ArticlesTableSeeder');
+		$this->call('PostsTableSeeder');
 	}
 
 }
